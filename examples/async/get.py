9fe764fe94c09b74700d8a593618738e12458ff1
Dmitry Volodin <dv@nocproject.org>
2024-02-27 10:56:00 +0100

    async get exampple

---
+++ examples/async/get.py
@@ -0,0 +1,16 @@
+import asyncio
+import sys
+from gufo.http.async_client import HttpClient
+
+
+async def main(url: str) -> None:
+    async with HttpClient() as client:
+        r = await client.get(url)
+        if r.status != 200:
+            print(f"Invalid response code: {r.status}")
+            return
+        data = await r.read()
+        print(data)
+
+
+asyncio.run(main(sys.argv[1]))
