7e2ca3266ddfb4ca3c897c4212b0132059d96eb0
Dmitry Volodin <dv@nocproject.org>
2025-03-14 17:40:18 +0100

    Fix PGO on busybox

--- tools/build/get-rustup-bin.sh
+++ tools/build/get-rustup-bin.sh
@@ -7,7 +7,7 @@ set +e
 
 RUSTUP=`which rustup`
 RUSTUP_HOME=`$RUSTUP show home`
-DEFAULT_HOST=`$RUSTUP show | grep "Default host" | sed -s 's/Default host: //'`
+DEFAULT_HOST=`$RUSTUP show | grep "Default host" | sed 's/Default host: //'`
 TOOLCHAIN=`$RUSTUP show active-toolchain | cut -d" " -f1`
 
 BIN="${RUSTUP_HOME}/toolchains/${TOOLCHAIN}/lib/rustlib/${DEFAULT_HOST}/bin"
