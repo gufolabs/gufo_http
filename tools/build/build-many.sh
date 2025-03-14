4c0ca57f469722e3774c40182832ccb96e54f6cd
Dmitry Volodin <dv@nocproject.org>
2025-03-14 15:05:33 +0100

    Fix build script

--- tools/build/build-many.sh
+++ tools/build/build-many.sh
@@ -1,15 +1,34 @@
 #!/bin/sh
 # ------------------------------------------------------------------------
-# Gufo Labs: Build wheel in the quay.io/pypa/manylinux2014_x86_64:latest
+# Build wheel in the manylinux
 # Usage:
-# ./tools/build/build-manylinux 3.8 3.9 3.10 3.11 3.12
+# ./tools/build/build-many 3.9 3.10 3.11 3.11 3.12 3.13
 # expects RUST_VERSION and RUST_ARCH
 # ------------------------------------------------------------------------
-# Copyright (C) 2022-24, Gufo Labs
+# Copyright (C) 2022-25, Gufo Labs
 # ------------------------------------------------------------------------
 
 # set -x
 set -e
+
+empty_dir() {
+    local path="$1"
+    if [ -d "${path}" ]; then
+        echo "Clearing ${path}..."
+        rm -rf "${path}"/*
+    else
+        echo "Creating ${path}..."
+        mkdir -p "${path}"
+    fi
+}
+
+ensure_dir() {
+    local path="$1"
+    if [ ! -x "${path}" ]; then
+        echo "Creating ${path}..."
+        mkdir -p "${path}"
+    fi
+}
 
 # Save base path
 BASE_PATH=$PATH
@@ -18,9 +37,17 @@ export RUSTUP_HOME=${RUSTUP_HOME:-/usr/local/rustup}
 export CARGO_HOME=${CARGO_HOME:-/usr/local/cargo}
 PATH=$CARGO_HOME/bin:$BASE_PATH
 
+# Paths
+BUILD="build"
+DIST="dist"
+TMP_WHEELHOUSE="/tmp/wheelhouse"
+WHEELHOUSE="wheelhouse"
+TARGET="target"
+
 echo "##"
 echo "## Installing rust"
 echo "##"
+empty_dir "${TARGET}"
 ./tools/build/setup-rust.sh
 rustup component add llvm-tools-preview
 
@@ -41,6 +68,7 @@ do
         3.10) ABI=cp310-cp310 ;;
         3.11) ABI=cp311-cp311 ;;
         3.12) ABI=cp312-cp312 ;;
+        3.13) ABI=cp313-cp313 ;;
         *)
             echo "Unknown Python version $1"
             exit 2
@@ -57,25 +85,31 @@ do
     # Check python
     PY_VER=$(python3 --version)
     echo "Python version: $PY_VER"
-    echo "Upgrade pip"
+    echo "Upgrade pip..."
     pip install --upgrade pip
-    echo "Setup build dependencies"
+    echo "Setup build dependencies..."
     pip install -r ./.requirements/build.txt -r ./.requirements/test.txt
-    # Collect PGO
+    echo "Collecting PGO..."
     PGO_DATA_DIR=`mktemp -d`
     ./tools/build/build-pgo.sh $PGO_DATA_DIR
-    # Build wheel
-    echo "Building wheel"
-    RUSTFLAGS="-Cprofile-use=$PGO_DATA_DIR/merged.profdata" python3 -m build --wheel
-    # Clean PGO
+    echo "Building wheel..."
+    empty_dir "${DIST}"
+    empty_dir "${BUILD}"
+    python3 -m build --wheel
+    echo "Clearing PGO..."
     rm -rf $PGO_DATA_DIR
-    #
-    echo "Auditing wheel"
-    auditwheel repair dist/*-$ABI-*.whl
-    echo "Installing wheel"
-    pip install wheelhouse/*-$ABI-*.whl
-    echo "Testing"
-    pytest -vv tests/
+    echo "Auditing wheel..."
+    empty_dir "${TMP_WHEELHOUSE}"
+    auditwheel repair --wheel-dir="${TMP_WHEELHOUSE}" "${DIST}"/*.whl
+    echo "Installing wheel..."
+    pip install "${TMP_WHEELHOUSE}"/*.whl
+    echo "Testing..."
+    pytest tests/
+    echo "Saving..."
+    ensure_dir "${WHEELHOUSE}"
+    cp "${TMP_WHEELHOUSE}"/*.whl "${WHEELHOUSE}"/
+    empty_dir "${DIST}"
+    empty_dir "${TMP_WHEELHOUSE}"
     echo "... done"
     shift
 done
