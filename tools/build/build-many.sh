4bca475d10c84120dedacb105ecbfd8bf25c828d
Dmitry Volodin <dv@nocproject.org>
2025-09-16 16:16:41 +0200

    Build only ARM64 wheels on MacOS

--- tools/build/build-many.sh
+++ tools/build/build-many.sh
@@ -118,6 +118,7 @@ do
         PV=$(ls $RUNNER_TOOL_CACHE/Python | grep "^$1" | sort -V | tail -n1)
         PATH=$CARGO_HOME/bin:$RUNNER_TOOL_CACHE/Python/$PV/arm64/bin:$BASE_PATH
         export PYO3_PYTHON=$RUNNER_TOOL_CACHE/Python/$PV/arm64/bin/python3
+        ARCHFLAGS="-arch arm64"
         pip install build delocate
     else
         # Linux
